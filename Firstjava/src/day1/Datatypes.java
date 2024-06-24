8ba168e423f04d20118e7310e4b7f83f1c1182d3
sharmagokul <145996111+sharmagokul@users.noreply.github.com>
2024-06-24 20:42:50 +0530

    ok

---
+++ Firstjava/src/day1/Datatypes.java
@@ -0,0 +1,51 @@
+package day1;
+
+public class Datatypes {
+	
+	public static void main(String[] args) {
+		
+		int a=100, b=200;
+		System.out.println("The value of a:"+ a);
+		System.out.println("The value of a:"+ b);
+		System.out.println("The sum of a and b is: "+(a+b));
+		
+		
+		byte c=125;
+		System.out.println(c);
+		
+		short d=3334;
+		System.out.println(d);
+		
+		long e=23456654646L;
+		System.out.println(e);
+		
+		//Decimal numbers == Float, double.
+		
+		float item_price = 15.5F;
+		System.out.println(item_price);
+		
+		double dbl= 1234.484764;
+		System.out.println(dbl);
+		
+		char grade= 'A';
+		System.out.println(grade);
+		
+		String name= "john";
+		System.out.println(name);
+		
+		//char ch='ABC';
+		//String ch ='ABC'
+		
+		
+		boolean b1= false;
+		System.out.println(b1);
+		
+		
+		
+				
+		
+		
+		
+	}
+
+}
---
+++ Firstjava/src/day1/Day1java.java
@@ -0,0 +1,72 @@
+package day1;
+
+public class Day1java {
+	public static void main(String[] args) {
+		
+		
+		
+		/*
+		int a; // Declaration
+		a=200;
+		System.out.println(a);
+		*/
+		
+		//int a=100;
+		//int b=200;
+		//int c=300;
+		
+		/*int a,b,c;
+		a=100;
+		b=200;
+		c=300;
+		System.out.println();
+		*/
+		
+		int a=100, b=200, c=300;
+		System.out.println("The value of a is:" + a);
+		System.out.println("The value of b is:" +b);
+		System.out.println("The value of c is:" +c );
+		
+		System.out.println(a+" "+b+ " "+c ); // 100 200 300
+		
+		// Data Types
+		// ==========
+		// Represents types of data.
+		// 1) Premitive 
+		// byte,short,int,long ======> number without decimal
+		// float,Double ==========> Decimal number.
+		// char ========> single character (single quote).
+		// boolean =======> True/False.
+		
+		
+		// 2) Non premitive/derived/ collection.
+		// String 
+		// Array list
+		// HashMap
+		// HashSet
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
